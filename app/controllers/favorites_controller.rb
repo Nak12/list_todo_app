53949cb712aa95dc5749cfb53c0c606f4102cbae
Kaio Masaru Nakazono <kaio.webtech@gmail.com>
2019-05-07 11:12:51 -0300

    editando o controller de Favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,30 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:show]
 
+  def index 
+    @favorites = Favorite.all 
+  end 
+
+  def show
+  end
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+    @favorite.list = List.find(params[:list])
+    @favorite.user = current_user
+    if @favorite.save
+      redirect_to lists_path #vai para index
+    else 
+      render list_path(params[:list]) #continua na mesma lista
+    end
+  end 
+
+  private
+    def favorite_params 
+      params.permit(:user_id, :list_id)
+    end 
+
+    def set_favorite
+      @favorite = Favorite.find(params[:id])
+    end
 end
